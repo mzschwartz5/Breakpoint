eaf1f74702cf3fbe29b3f8e86406bbf19f990383
Daniel <dgerh@sas.upenn.edu>
2024-11-02 09:35:44 -0400

    cmake and directory refactoring

--- src/Support/pch.h
+++ src/Support/pch.h
@@ -4,7 +4,7 @@
 #include <wrl/client.h>
 #include <dxgi1_4.h>
 #include <d3d12.h>
-#include "d3dx12.h"
+#include "../D3D/d3dx12.h"
 #include <pix.h>
 #include <DirectXColors.h>
 #include <DirectXMath.h>
