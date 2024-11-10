96f5523eba31f5333e05cf58f7d680d451acc802
Zixiao Wang <95837786+Lanbiubiu1@users.noreply.github.com>
2024-11-09 19:41:19 -0500

    Update RootSignature.hlsl

--- src/Shaders/RootSignature.hlsl
+++ src/Shaders/RootSignature.hlsl
@@ -6,5 +6,6 @@
 "RootFlags(ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT)," \
 "DescriptorTable(UAV(u0, numDescriptors = 2)),"  /* UAVs at u0 and u1 */ \
 "RootConstants(num32BitConstants=32, b0),"       /* For view and projection matrices */ \
-"DescriptorTable(SRV(t0))"                       /* SRV for model matrices buffer */
+"DescriptorTable(SRV(t0, numDescriptors = 1)),"  /* SRV for model matrices buffer */ \
+"DescriptorTable(CBV(b1, numDescriptors = 1))" 
 
