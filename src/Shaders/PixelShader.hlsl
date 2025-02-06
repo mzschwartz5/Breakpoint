a879357241b44ac02b683eb0c4f8e2f3bb63a409
Matt Schwartz <mzschwartz5@gmail.com>
2025-02-06 16:55:29 -0500

    Basic shading on beach object

--- src/Shaders/PixelShader.hlsl
+++ src/Shaders/PixelShader.hlsl
@@ -8,5 +8,12 @@ struct PSInput {
 [RootSignature(ROOTSIG)]
 float4 main(PSInput input) : SV_Target
 {
-    return input.normal;
+    float4 baseColor = float4(0.8, 0.7, 0.6, 1);
+
+    // Basic lambertian shading
+    float3 lightDir = normalize(float3(1, 1, 1));
+    float3 normal = normalize(input.normal.xyz);
+    float intensity = saturate(dot(normal, lightDir));
+    float4 ambientLight = float4(0.2, 0.2, 0.2, 1);
+    return ambientLight + baseColor * float4(intensity, intensity, intensity, 1);
 }
